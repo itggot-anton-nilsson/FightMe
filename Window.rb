ce9971845748717848b13e9a887a56a83a91d437
itggot-anton-nilsson <anton.nilsson5@ITGGOT-OR4FLNFB.learnet.se>
2016-10-26 13:45:25 +0200

    First

---
+++ Window.rb
@@ -0,0 +1,15 @@
+class Window < Gosu::Window
+
+  def initialize
+    super 640, 480, false
+
+  end
+
+  def draw
+
+  end
+
+  def update
+
+  end
+end
---
+++ main.rb
@@ -0,0 +1,5 @@
+require 'gosu'
+require_relative 'Window'
+
+wd = Window.new
+wd.show
