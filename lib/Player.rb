e90cd2b61bbf9bbc620f2e25a97197b557a2fa39
itggot-anton-nilsson <anton.nilsson@itggot.se>
2016-10-27 16:35:12 +0200

    Shit moves

--- lib/Player.rb
+++ lib/Player.rb
@@ -1,13 +1,20 @@
 class Player
-  def initialize
-
+  def initialize(x)
+    @x = x
+    @y = 350
+    @guy = Gosu::Image.new('Media/Guy.png')
   end
 
   def draw
-
+    @guy.draw(@x, @y, 0)
   end
 
   def update
 
   end
+
+  def walk(number)
+    @x += number
+  end
+
 end
--- lib/Window.rb
+++ lib/Window.rb
@@ -4,15 +4,21 @@ class Window < Gosu::Window
     super 640, 480, false
     self.caption = 'A great motherfucking game'
     @back_image = Gosu::Image.new('Media/Dojo.png', :tileable => true)
-    @player1 = Player.new
+    @player1 = Player.new(40)
+    @player2 = Player.new(600)
   end
 
   def draw
     @back_image.draw(0, 0, 0)
     @player1.draw
+    @player2.draw
   end
 
   def update
 
+  end
+
+  def button_down(id)
+     Movement.button_down(id, @player1, @player2)
   end
 end
